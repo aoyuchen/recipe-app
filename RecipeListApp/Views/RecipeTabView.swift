dbbd62c51baa6400689d1e4badcf28d0a22edac0
Aoyu Chen <aoyuchen76@gmail.com>
2021-05-22 20:09:01 -0400

    Add tabview as the entry point of the app

---
+++ RecipeListApp/Views/RecipeTabView.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeTabView.swift
+//  RecipeListApp
+//
+//  Created by Aoyu Chen on 5/22/21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
